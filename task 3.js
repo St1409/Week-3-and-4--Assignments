4c3cc83d55552951f2905937c9da77f854f7da43
St1409 <93860364+St1409@users.noreply.github.com>
2026-07-01 21:30:36 +0530

    Add files via upload

---
+++ task 3.js
@@ -0,0 +1,16 @@
+if (num1 > num2) {
+    console.log("Larger number is:", num1);
+} else if (num2 > num1) {
+    console.log("Larger number is:", num2);
+} else {
+    console.log("Both numbers are equal.");
+}
+
+let number = 11;
+
+if (number % 2 === 0) {
+    console.log(number + " is an Even number.");
+} else {
+    console.log(number + " is an Odd number.");
+}
+
